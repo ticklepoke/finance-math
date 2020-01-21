d229d5968744d93d05a2f397457905eb243afc1a
Nigel Lee <lee.nigel@hotmail.com>
2020-01-21 19:22:28 +0800

    Updates

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,3 @@
 var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"PV","url":"index.html#pv","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"FV","url":"index.html#fv","classes":"tsd-kind-function"}]};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"PVSingleCashFlow","url":"index.html#pvsinglecashflow","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"FVSingleCashFlow","url":"index.html#fvsinglecashflow","classes":"tsd-kind-function"}]};
